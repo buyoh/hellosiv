604ff84fb09deb364b2728579d09bc2dffb86df9
舞葉 <japanjetone@live.jp>
2019-11-26 09:45:57 +0900

    modify car

--- hellosiv/Car.h
+++ hellosiv/Car.h
@@ -4,15 +4,20 @@
 
 
 class Car {
+    double engine_ = 0.0;
 	double accsel_;
 	double steer_;
 	P2Body body_;
-    static constexpr double CAccselForce = 5;
+    static constexpr double CEngineForce = 1.0;
+    static constexpr double CEngineDump = 0.99;
+    static constexpr double CAccselEngine = 0.1;
+    static constexpr double CSteerFront = 0.4;
+    static constexpr double CSteerRear = -0.1;
 public:
 
     void initialize() {
-        body_.setDamping(0.9);
-        body_.setAngularDamping(0.99);
+        body_.setDamping(1.4);
+        body_.setAngularDamping(2);
     }
 
 	void setAccsel(double _accsel) {
@@ -28,12 +33,22 @@ public:
     void apply() {
         // const auto t = Transformer2D(Mat3x2::Rotate(body_.getAngle()).translated(body_.getPos()));
 
+        engine_ += CAccselEngine *accsel_;
+        engine_ *= CEngineDump;
+        //if (body_.getVelocity().lengthSq() != 0)
+        //    engine_ *= Math::Pow(Math::Abs(body_.getVelocity().normalized().dot(Vec2::UnitY().rotated(body_.getAngle()))), 4);
+
+        auto side = body_.getVelocity().dot(-Vec2::UnitX().rotated(body_.getAngle()));
+        body_.applyForce(0.05*side* Vec2::UnitX().rotated(body_.getAngle()));
+
+        Print << engine_ << U" " << side;
+
         body_.applyForce(
-            Vec2{ 0, CAccselForce*accsel_ }.rotate(body_.getAngle()),
+            Vec2{ 0, CEngineForce * engine_ }.rotate(body_.getAngle() + CSteerRear*steer_),
             Vec2{ 0, -0.5 }.rotate(body_.getAngle())
         );
         body_.applyForce(
-            Vec2{ 0, CAccselForce * accsel_ }.rotate(body_.getAngle()+0.4*steer_),
+            Vec2{ 0, CEngineForce * engine_ }.rotate(body_.getAngle()+ CSteerFront *steer_),
             Vec2{ 0, 0.5 }.rotate(body_.getAngle())
         );
         // body_.applyTorque(2.0*steer_);
--- hellosiv/Main.cpp
+++ hellosiv/Main.cpp
@@ -21,11 +21,6 @@ void Main()
     Car carBody{ world.createPolygons({0, 0}, carPolygon, P2Material(0.1, 0.0, 1.0)) };
     Texture carTex = Texture(Emoji(U"⬇"));
 
-    Polygon polygon;
-
-
-    P2Body p;
-    P2Body p2 = world.createStaticRect({10, 20}, SizeF{ 20, 10 }, P2Material(1, 0.1, 1.0));
 
     Field field{ {0.0,0.0}, {100.0, 0.0}, {100.0, 100.0}, {-100.0, 100.0}, {-100.0, 0.0} };
     field.generatePolygonFromPath();
@@ -40,22 +35,6 @@ void Main()
             const auto t = camera.createTransformer();
 
 
-            const Circle shape(Arg::center(Cursor::Pos()), 3);
-            shape.drawFrame(2, Palette::Skyblue);
-
-            if (MouseL.pressed())
-            {
-                // マップの Polygon に円を追加
-                polygon.append(shape.asPolygon());
-            }
-
-            if (KeySpace.down()) {
-                polygon = polygon.simplified(2.0);
-                p = world.createStaticClosedLineString({ 0, 0 }, LineString(polygon.vertices().map([](Float2 f) { return Vec2{ f.x, f.y }; })));
-                //p = world.createStaticPolygon({ 0, 0 }, polygon, P2Material(0.1, 0.0, 1.0));
-               
-            }
-
             {
                 carBody.setAccsel(KeyUp.pressed() ? 1 : KeyDown.pressed() ? -1 : 0);
                 carBody.setSteer(KeyLeft.pressed() ? -1 : KeyRight.pressed() ? 1 : 0);
@@ -69,10 +48,6 @@ void Main()
 
             field.polygon().draw(Palette::Gray);
 
-            polygon.draw(Palette::Gray);
-            p2.draw();
-
-            p.draw();
             carBody.body().draw();
 
             for (int i = -1; i <= 1; ++i) {
